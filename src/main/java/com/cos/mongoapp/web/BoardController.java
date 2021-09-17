256368e4d23b1af3a0ac75ca6e91c4bde1a15546
ssar <ssar@nate.com>
2021-09-17 17:48:35 +0900

    update, delete, 한건찾기추가

--- src/main/java/com/cos/mongoapp/web/BoardController.java
+++ src/main/java/com/cos/mongoapp/web/BoardController.java
@@ -4,8 +4,11 @@ package com.cos.mongoapp.web;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,32 @@ import lombok.RequiredArgsConstructor;
 public class BoardController {
 	//DI
 	private final BoardRepository boardRepository;
+	
+	@PutMapping("/board/{id}")
+	public Board update(@RequestBody BoardSaveDto dto, @PathVariable String id) {
+		
+		Board board = dto.toEntity();
+		board.set_id(id); // save함수는 같은 아이디면 수정한다.
+		
+		return boardRepository.save(board);
+		
+		//postman put delete쓸려고 사용하는거임 ㅎㅎ
+	}
+	
+	
+	@DeleteMapping("/board/{id}")
+	public int deleteById(@PathVariable String id){
+		boardRepository.deleteById(id);
+		
+		return 1;  //1 성공, -1 실패
+	}
+	
+	
+	@GetMapping("/board/{id}")
+	public Board findById(@PathVariable String id) {
+		return  boardRepository.findById(id).get();
+	}
+	
 	
 	@GetMapping("/board")
 	public List<Board>findAll() { //리턴을 JAVAObject로 하면 스프링 내부적으로 JSON으로 자동 변환해준다.
